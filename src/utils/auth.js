cb0d04be2b6219aad7f56f562bdd9408ecb5ebcf
IxxyDev <denisenko@uchi.ru>
2020-11-17 14:45:02 +0300

    feat: make auth methods

---
+++ src/utils/auth.js
@@ -0,0 +1,50 @@
+import {setToken} from "./token";
+
+export const BASE_URL = 'https://api.nomoreparties.co'
+
+export const register = (email, password) => {
+	return fetch(`${BASE_URL}/signup`, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({email, password})
+	})
+		.then(res => res.json())
+		.then(res => res)
+		.catch(err => console.error(err))
+}
+
+export const authorize = (email, password) => {
+	return fetch(`${BASE_URL}/signin`, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({email, password})
+	})
+		.then(res => res.json())
+		.then(data => {
+			if (data.user) {
+				setToken(data.jwt)
+				return data
+			} else {
+				return
+			}
+		})
+		.catch(err => console.error(err))
+}
+
+export const getContent = token => {
+	return fetch(`${BASE_URL}/users/me`, {
+		method: 'GET',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${token}`,
+		}
+	})
+		.then(res => res.json())
+}
--- src/utils/token.js
+++ src/utils/token.js
@@ -2,4 +2,4 @@ const TOKEN_KEY = "jwt"
 
 export const getToken = () => localStorage.getItem(TOKEN_KEY)
 export const setToken = token => localStorage.setItem(TOKEN_KEY, token)
-export const removeToken = token => localStorage.removeItem(TOKEN_KEY)
+export const removeToken = () => localStorage.removeItem(TOKEN_KEY)
